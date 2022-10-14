4425d2eb5b8267de36aa1f3e47a29c3dcd375985
kjtkanaly <kjtkanaly@gmail.com>
2022-10-14 10:48:07 -0400

    Getting acquanted with html and css

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let name = "Beep";
-greet(name);
+function pupUpHi()
+{
+    alert("Hello");
+}
 
 function greet(name) {
     console.log('hello ' + name);
 }
+
+////////////////////////////////////////////
+/*
+let timeDelay = 2000;
+
+document.body.style.background = "red"
+
+setTimeout(() => document.body.style.background = "", timeDelay);
+*/
